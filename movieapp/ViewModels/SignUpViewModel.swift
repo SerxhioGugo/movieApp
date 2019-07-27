f8829934467904efa66194893d856e7fc811eedf
SerxhioGugo <serxhio.gugo@gmail.com>
2019-07-26 23:08:39 -0400

    Storing Image + Info

--- movieapp/ViewModels/SignUpViewModel.swift
+++ movieapp/ViewModels/SignUpViewModel.swift
@@ -22,6 +22,7 @@ class SignUpViewModel {
     func performSignUp(completion: @escaping (Error?) -> Void) {
         guard let email = email, let password = password else { return }
         bindableIsSigningUp.value = true
+        
         Auth.auth().createUser(withEmail: email, password: password) { (res, err) in
             
             if let error = err {
@@ -30,25 +31,46 @@ class SignUpViewModel {
             }
             
             print("Successfully registered user: ", res?.user.uid ?? "")
-            let filename = UUID().uuidString
-            let ref = Storage.storage().reference(withPath: "/images/\(filename)")
-            let imageData = self.bindableImage.value?.jpegData(compressionQuality: 0.75) ?? Data()
-            ref.putData(imageData, metadata: nil, completion: { (_, err) in
-                
+            
+            self.saveImageToFirebase(completion: completion)
+        }
+    }
+    
+    fileprivate func saveImageToFirebase(completion: @escaping (Error?) -> Void) {
+        let filename = UUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/images/\(filename)")
+        let imageData = self.bindableImage.value?.jpegData(compressionQuality: 0.75) ?? Data()
+        ref.putData(imageData, metadata: nil, completion: { (_, err) in
+            
+            if let err = err {
+                completion(err)
+                return
+            }
+            print("Finished uploading image to storage")
+            ref.downloadURL(completion: { (url, err) in
                 if let err = err {
                     completion(err)
                     return
                 }
-                print("Finished uploading image to storage")
-                ref.downloadURL(completion: { (url, err) in
-                    if let err = err {
-                        completion(err)
-                        return
-                    }
-                    self.bindableIsSigningUp.value = false
-                    print("Download url for image : ", url?.absoluteString ?? "")
-                })
+                self.bindableIsSigningUp.value = false
+                print("Download url for image : ", url?.absoluteString ?? "")
+                let imageUrl = url?.absoluteString ?? ""
+                self.saveInfoToFirestore(imageUrl: imageUrl, completion: completion)
+                completion(nil)
             })
+        })
+
+    }
+    
+    fileprivate func saveInfoToFirestore(imageUrl: String, completion: @escaping (Error?) -> Void) {
+        let uid = Auth.auth().currentUser?.uid ?? ""
+        let docData = ["name": name ?? "" , "uid": uid, "imageUrl": imageUrl]
+        Firestore.firestore().collection("users").document(uid).setData(docData) { err in
+            if let err = err {
+                completion(err)
+                return
+            }
+            completion(nil)
         }
     }
     
